d6d811b43a98a57e394c09297600f344117593e8
Darko <darkosarajkic997@gmail.com>
2021-05-20 21:42:34 +0200

    Gitignore updated and keyboard added

--- app.py
+++ app.py
@@ -33,34 +33,100 @@ viber = Api(BotConfiguration(
     auth_token=os.getenv('AUTH_TOKEN')
 ))
 
+keyboard = {
+            "DefaultHeight": False,
+            "BgColor": "#222222",
+            "Type": "keyboard",
+            "Buttons": [{
+		"Columns": 2,
+		"Rows": 2,
+		"Text": "<br><font color=\"#494E67\"><b>ASIAN</b></font>",
+		"TextSize": "large",
+		"TextHAlign": "center",
+		"TextVAlign": "middle",
+		"ActionType": "reply",
+		"ActionBody": "ASIAN",
+		"BgColor": "#f7bb3f",
+		"Image": "https://s18.postimg.org/9tncn0r85/sushi.png"
+	}, {
+		"Columns": 2,
+		"Rows": 2,
+		"Text": "<br><font color=\"#494E67\"><b>FRENCH</b></font>",
+		"TextSize": "large",
+		"TextHAlign": "center",
+		"TextVAlign": "middle",
+		"ActionType": "reply",
+		"ActionBody": "French",
+		"BgColor": "#7eceea",
+		"Image": "https://s18.postimg.org/ntpef5syd/french.png"
+	}, {
+		"Columns": 2,
+		"Rows": 2,
+		"Text": "<br><font color=\"#494E67\"><b>MEXICAN</b></font>",
+		"TextSize": "large",
+		"TextHAlign": "center",
+		"TextVAlign": "middle",
+		"ActionType": "reply",
+		"ActionBody": "Mexican",
+		"BgColor": "#f6f7f9",
+		"Image": "https://s18.postimg.org/t8y4g4kid/mexican.png"
+	}, {
+		"Columns": 2,
+		"Rows": 2,
+		"Text": "<br><font color=\"#494E67\"><b>ITALIAN</b></font>",
+		"TextSize": "large",
+		"TextHAlign": "center",
+		"TextVAlign": "middle",
+		"ActionType": "reply",
+		"ActionBody": "Italian",
+		"BgColor": "#dd8157",
+		"Image": "https://s18.postimg.org/x41iip3o5/itallian.png"
+	}, {
+		"Columns": 2,
+		"Rows": 2,
+		"Text": "<br><font color=\"#494E67\"><b>INDIE</b></font>",
+		"TextSize": "large",
+		"TextHAlign": "center",
+		"TextVAlign": "middle",
+		"ActionType": "reply",
+		"ActionBody": "Indie",
+		"BgColor": "#f6f7f9",
+		"Image": "https://s18.postimg.org/wq06j3jkl/indi.png"
+	}, {
+		"Columns": 2,
+		"Rows": 2,
+		"Text": "<br><font color=\"#494E67\"><b>MORE</b></font>",
+		"TextSize": "large",
+		"TextHAlign": "center",
+		"TextVAlign": "middle",
+		"ActionType": "reply",
+		"ActionBody": "More",
+		"BgColor": "#a8aaba",
+		"Image": "https://s18.postimg.org/ylmyu98et/more_Options.png"
+	}]
+        }
+
 
 @app.route('/', methods=['POST'])
 def incoming():
-    logger.debug("received request. post data: {0}".format(request.get_data()))
+    #logger.debug("received request. post data: {0}".format(request.get_data()))
 
     viber_request = viber.parse_request(request.get_data().decode('utf8'))
-
+    
     if isinstance(viber_request, ViberMessageRequest):
-        message = viber_request.message
-        message1=TextMessage(None,None,'Jedi govna',None)
-        viber.send_messages(viber_request.sender.id, [
-            message1
-        ])
-    elif isinstance(viber_request, ViberConversationStartedRequest) \
-            or isinstance(viber_request, ViberSubscribedRequest) \
-            or isinstance(viber_request, ViberUnsubscribedRequest):
+        message = TextMessage(text='Cao',keyboard=keyboard)
+        viber.send_messages(viber_request.sender.id, [message])
+    elif isinstance(viber_request, ViberConversationStartedRequest) or isinstance(viber_request, ViberSubscribedRequest) or isinstance(viber_request, ViberUnsubscribedRequest):
         viber.send_messages(viber_request.sender.id, [
             TextMessage(None, None, viber_request.get_event_type(),None)
         ])
     elif isinstance(viber_request, ViberFailedRequest):
-        logger.warn(
-            "client failed receiving message. failure: {0}".format(viber_request))
+        logger.warn("client failed receiving message. failure: {0}".format(viber_request))
 
     return Response(status=200)
 
 
 def set_webhook(viber):
-    print(os.getenv('WEBHOOK_URL'))
     viber.set_webhook(os.getenv('WEBHOOK_URL'))
 
 
